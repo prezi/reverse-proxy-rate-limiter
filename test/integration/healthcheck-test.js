ad5c518dba6ae218bf59302f4e67a6e6f214bc94
tkornai <tamas.kornai@prezi.com>
2015-03-04 11:43:40 +0100

    Wrap health check test with describe block

--- test/integration/healthcheck-test.js
+++ test/integration/healthcheck-test.js
@@ -4,27 +4,32 @@ var rateLimiter = require("./../../index.js"),
     request = require("request"),
     assert = require("assert");
 
-it("should return a healthcheck if healthcheck header is set", function (done) {
+describe("Healthcheck test", function() {
     var host = "localhost";
     var listenPort = 8082;
 
-    rateLimiter.createRateLimiter({
-        listenPort: listenPort,
-        forwardPort: 8081,
-        forwardHost: host,
-        configRefreshInterval: 10000,
-        configEndpoint: 'file:./test/fixtures/example_configuration.json'
+    var rl;
+    before(function() {
+        rl = rateLimiter.createRateLimiter({
+            listenPort: listenPort,
+            forwardPort: 8081,
+            forwardHost: host,
+            configRefreshInterval: 10000,
+            configEndpoint: 'file:./test/fixtures/example_configuration.json'
+        });
     });
 
-    request({
-        url: "http://" + host + ":" + listenPort,
-        headers: {
-            'x-rate-limiter': 'healthcheck'
-        }
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            assert.equal(body, "OK");
-            done();
-        }
+    it("should return a healthcheck if healthcheck header is set", function (done) {
+        request({
+            url: "http://" + host + ":" + listenPort,
+            headers: {
+                'x-rate-limiter': 'healthcheck'
+            }
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                assert.equal(body, "OK");
+                done();
+            }
+        });
     });
 });
