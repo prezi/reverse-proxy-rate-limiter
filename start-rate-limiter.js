f3737302431eda4ebc2aa6a02f4076a70d01c431
Zoltan Nagy <abesto@abesto.net>
2015-03-06 16:31:26 +0100

    Such Config!

--- start-rate-limiter.js
+++ start-rate-limiter.js
@@ -1,6 +1,26 @@
-var fs = require('fs'),
-    rateLimiter = require("./index.js");
+/* global require */
+(function () {
+    "use strict";
+    var fs = require('fs'),
+        rateLimiter = require('./index'),
+        _ = require('lodash');
 
-var config = JSON.parse(fs.readFileSync('/etc/prezi/ratelimiter/configuration.json', 'utf8'));
+    var opts = require('nomnom')
+        .option('config', {
+            abbr: 'c',
+            default: null,
+            help: 'Configuration file to use'
+        })
+        .option('version', {
+            abbr: 'v',
+            flag: true,
+            help: 'Print version and exit',
+            callback: function () {
+                return "version " + require('./package.json').version;
+            }
+        })
+        .parse();
 
-rateLimiter.createRateLimiter(config);
+    var config = require('./lib/rate-limiter/config').load(opts.config);
+    var rl = rateLimiter.createRateLimiter(config);
+}());
