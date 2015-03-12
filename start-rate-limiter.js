0b57dbd4370a34da3ae6e73fb7e398c2c31bf80a
Daniel Torok <daniel.torok@prezi.com>
2015-03-12 18:28:45 +0100

    [bugfix] config->settings bugfix

--- start-rate-limiter.js
+++ start-rate-limiter.js
@@ -1,5 +1,5 @@
 /* global require */
-var config = require('./lib/rate-limiter/config').init(),
+var config = require('./lib/rate-limiter/settings').init(),
     rateLimiter = require('./lib/rate-limiter');
 
 rateLimiter.createRateLimiter(config);
