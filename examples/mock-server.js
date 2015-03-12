4a7505de4f9fc1b3e8d8d99b3ff957ff23664566
Daniel Torok <daniel.torok@prezi.com>
2015-03-12 18:20:23 +0100

    [docs] example files added

---
+++ examples/mock-server.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict"; 
+
+    var http = require("http");
+
+    function handler(req, res) {
+        var status_code = 200;
+
+        var body = "";
+        body += "Hello ratelimiter!\n";
+        body += req.url + "\n";
+        body += JSON.stringify(req.headers, true, 2);
+
+        res.writeHead(status_code, {'Content-Type': 'text/plain'});
+        res.write(body);
+        res.end();
+
+
+        console.log(["request", req.url, status_code].join(" "))
+    }
+
+    http.createServer(function (req, res) {
+        if (req.url == "/sleep5secs/") {
+            setTimeout(handler, 5000, req, res);
+        } else {
+            handler(req, res);
+        }
+    }).listen(7001);
+})()
